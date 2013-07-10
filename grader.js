42d3085c8e291c1836f32655838b78f5c9da9d24
ashwin moily <ashwinmoily@gmail.com>
2013-07-10 14:26:50 +0000

    Updating grader.js

--- grader.js
+++ grader.js
@@ -26,6 +26,20 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+var URL_DEF = "http://secret-anchorage-3176.herokuapp.com/";
+
+var assertURLExists = function(url, infile) {
+    if (url == URL_DEF) {
+	return assertFileExists(infile);
+ }
+  else {
+ var path = url + "/" + infile;
+ rest.get(path).on('complete', function(ff) {
+    return assertFileExists(ff);
+	});
+    }
+}
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,6 +79,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url ', 'Remote URL', assertURLExists, URL_DEF)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
